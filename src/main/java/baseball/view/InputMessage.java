14d2f7e277591752b3c914e6c7f02ba586a9a93f
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 16:04:11 +0900

    style(view): reformat line

--- src/main/java/baseball/view/InputMessage.java
+++ src/main/java/baseball/view/InputMessage.java
@@ -4,7 +4,6 @@ import static baseball.util.Constants.FLAG_GAME_RESTART;
 import static baseball.util.Constants.FLAG_GAME_QUIT;
 
 public enum InputMessage {
-
     NUMBER("숫자를 입력해주세요 : "),
     GAME_COMMAND(String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.",
             FLAG_GAME_RESTART, FLAG_GAME_QUIT));
