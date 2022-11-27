c62701803ff39093d1879f5626eaf82a00cc378f
sudo-dustle <asdf748596123@gmail.com>
2022-11-27 15:42:11 +0900

    feat: Input, Output 생성

---
+++ src/main/java/util/Input.java
@@ -0,0 +1,36 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Input {
+    private final InputValidator inputValidator;
+
+    public Input(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public List<Integer> getUserNumbers() {
+        String readLine = Console.readLine();
+
+        inputValidator.validateDigits(readLine);
+
+        List<Integer> numbers = Arrays.stream(readLine.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        inputValidator.validateInputSize(numbers);
+        inputValidator.validateNumberRange(numbers);
+        return numbers;
+    }
+
+    public String getCommandAfterGameWin() {
+        String readCommand = Console.readLine();
+        inputValidator.validateDigits(readCommand);
+        inputValidator.validateCommandAfterGame(readCommand);
+        return readCommand;
+    }
+}
