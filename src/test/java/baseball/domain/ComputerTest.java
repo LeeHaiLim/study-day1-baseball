87ae97c6dcf8abb939c35eb7aeaa94c6eac6c6c8
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 16:39:58 +0900

    test: 테스트 중복 제거

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -1,17 +1,23 @@
 package baseball.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class ComputerTest {
+    private Computer computer;
+    
+    @BeforeEach
+    void init() {
+        computer = Computer.saveComputerNumber(List.of(1, 2, 3));
+    }
 
     @DisplayName("1개의 자리가 일치하면 1스트라이크")
     @Test
     void compareResultStrikeTest() {
-        Computer computer = Computer.saveComputerNumber(List.of(1, 2, 3));
         Counting counting = computer.compareResult(List.of(1, 4, 5));
         Assertions.assertThat(counting.toString()).isEqualTo("1스트라이크");
     }
@@ -19,7 +25,6 @@ class ComputerTest {
     @DisplayName("3개가 포함되어 있으면 3볼")
     @Test
     void compareResultBallTest() {
-        Computer computer = Computer.saveComputerNumber(List.of(1, 2, 3));
         Counting counting = computer.compareResult(List.of(3, 1, 2));
         Assertions.assertThat(counting.toString()).isEqualTo("3볼");
     }
@@ -27,7 +32,6 @@ class ComputerTest {
     @DisplayName("모두 일치하면 3스트라이크")
     @Test
     void compareResultSuccessTest() {
-        Computer computer = Computer.saveComputerNumber(List.of(1, 2, 3));
         Counting counting = computer.compareResult(List.of(1, 2, 3));
         Assertions.assertThat(counting.toString()).isEqualTo("3스트라이크");
     }
@@ -35,7 +39,6 @@ class ComputerTest {
     @DisplayName("2볼 1스트라이크")
     @Test
     void compareResultStrikeBallTest() {
-        Computer computer = Computer.saveComputerNumber(List.of(1, 2, 3));
         Counting counting = computer.compareResult(List.of(1, 3, 2));
         Assertions.assertThat(counting.toString()).isEqualTo("2볼 1스트라이크");
     }
