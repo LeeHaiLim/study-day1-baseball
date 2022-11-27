460e4d8aa46fda4c0cf43b28d559723ec248eba6
Limjongho <whdgh9873@icloud.com>
2022-11-27 13:04:34 +0900

    feat(NumberGenerator): 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/util/NumberGenerator.java
+++ src/main/java/baseball/util/NumberGenerator.java
@@ -1,10 +1,22 @@
 package baseball.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NumberGenerator {
 
+    private static final int MiN_NUM = 1;
+    private static final int MAX_NUM = 9;
+
     public static List<Integer> getGameNumbers() {
-        return null;
+        Set<Integer> randomNumbers = new HashSet<>();
+        while(randomNumbers.size()<3) {
+            randomNumbers.add(Randoms.pickNumberInRange(MiN_NUM, MAX_NUM));
+        }
+        return new ArrayList<>(randomNumbers);
     }
 }
