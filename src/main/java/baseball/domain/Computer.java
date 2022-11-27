ba7cd3308989dcb7b28b903566d68436b17d7fb6
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 15:04:27 +0900

    feat: 랜덤 숫자를 Computer에 저장하는 메소드 생성

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Computer {
     private final List<Integer> threeNumbers;
 
-    public Computer(List<Integer> threeNumbers) {
+    private Computer(List<Integer> threeNumbers) {
         this.threeNumbers = threeNumbers;
+    }
+
+    public static Computer saveComputerNumber(List<Integer> threeNumbers) {
+        return new Computer(threeNumbers);
     }
 }
