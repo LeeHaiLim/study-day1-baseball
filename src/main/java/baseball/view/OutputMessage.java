5e3219981681a17cc372e1f670a5b7b68b7b8e0f
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:13:53 +0900

    feat(view): add string constants
    Added string constants:
    - ball output message
    - strike output message
    - nothing output message

--- src/main/java/baseball/view/OutputMessage.java
+++ src/main/java/baseball/view/OutputMessage.java
@@ -1,10 +1,14 @@
 package baseball.view;
 
-import baseball.Constants;
+import static baseball.util.Constants.BASEBALL_NUMBER_SIZE;
 
 public enum OutputMessage {
     GAME_START("숫자 야구 게임을 시작합니다."),
-    GAME_END(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", Constants.BASEBALL_NUMBER_SIZE));
+    GAME_END(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", BASEBALL_NUMBER_SIZE)),
+    BALL("%d볼 "),
+    STRIKE("%d스트라이크"),
+    NOTHING("낫싱")
+    ;
 
     private final String message;
 
