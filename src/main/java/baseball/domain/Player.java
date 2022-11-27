cd03c12a053398a70c68f83c37c632320bf95346
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 12:52:08 +0900

    feat: Player 객체 생성

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Player {
+    private final List<Integer> numbers;
+
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
