5b60731aad6176b441fbd4f2e905a550d8f84e9e
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 12:37:12 +0900

    feat(baseball): implement class constants
    Added int constants:
    - baseball number range start
    - baseball number range end
    - baseball number size

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Constants {
+    public static final int BASEBALL_NUMBER_RANGE_START = 1;
+    public static final int BASEBALL_NUMBER_RANGE_END = 9;
+    public static final int BASEBALL_NUMBER_SIZE = 3;
+}
