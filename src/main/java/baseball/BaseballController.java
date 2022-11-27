a58e7353283278eb925c1c10bed824d4265d232e
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:36:24 +0900

    feat(baseball): implement class baseball controller
    Added methods:
    - run
    - playGame

---
+++ src/main/java/baseball/BaseballController.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import baseball.domain.Answer;
+import baseball.domain.GameCommand;
+import baseball.domain.Guess;
+import baseball.domain.Result;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void run() {
+        do {
+            Answer answer = Answer.generateAnswer();
+            playGame(answer);
+        } while (new GameCommand(inputView.readInputGameCommand()).isRestart());
+
+    }
+
+    public void playGame(Answer answer) {
+        Guess guess;
+        Result result;
+        do {
+            guess = new Guess(inputView.readInputNumber());
+            result = new Result(answer, guess);
+            outputView.printResult(result.getBall(), result.getStrike());
+        } while (!result.isWin());
+    }
+}
