3a9958b198f5141da4099a10cc80b6e32d02a706
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:27:55 +0900

    refactor(view): change variable name

--- src/main/java/baseball/view/InputMessage.java
+++ src/main/java/baseball/view/InputMessage.java
@@ -6,7 +6,7 @@ import static baseball.util.Constants.FLAG_GAME_QUIT;
 public enum InputMessage {
 
     NUMBER("숫자를 입력해주세요 : "),
-    QUIT_OR_RESTART(String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.",
+    GAME_COMMAND(String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.",
             FLAG_GAME_RESTART, FLAG_GAME_QUIT));
 
     private final String message;
