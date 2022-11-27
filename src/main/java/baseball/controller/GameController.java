44e27ced04db4224c6592ad4821d496533831ef0
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 16:16:05 +0900

    feat: 게임 시작 및 재시작 메서드 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,11 +1,41 @@
 package baseball.controller;
 
+import baseball.domain.Command;
+import baseball.domain.Computer;
+import baseball.domain.Counting;
+import baseball.domain.Player;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private Computer computer;
+
+    public void startGame() {
+        boolean isContinue;
+        do {
+            computer = Computer.saveComputerNumber(generateThreeNumber());
+            repeatGame();
+            outputView.printFinishMessage();
+            Command command = inputView.readGameCommand();
+            isContinue = command.isRestart();
+        } while (isContinue);
+    }
+
+    private void repeatGame() {
+        boolean isSuccess = false;
+        while (!isSuccess) {
+            Player player = new Player(inputView.readNumbers());
+            Counting counting = computer.compareResult(player.getNumbers());
+            outputView.printResult(counting.toString());
+            isSuccess = counting.isFinish();
+        }
+    }
 
     public List<Integer> generateThreeNumber() {
         List<Integer> computerNumbers = new ArrayList<>();
