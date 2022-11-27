01a9700362c5f35a9ef811c652d06501e0e1e1f4
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 16:04:38 +0900

    refactor(baseball): remove exception handling logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         BaseballController controller = new BaseballController();
-        try {
-            controller.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+        controller.run();
     }
 }
