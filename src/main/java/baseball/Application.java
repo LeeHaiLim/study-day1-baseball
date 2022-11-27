1398448be0a8bf53c12ea2a53f45eb8463ce30a6
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 16:15:44 +0900

    feat: 게임 시작 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.constant.Message;
+import baseball.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(Message.START_GAME_MESSAGE);
+        GameController gameController = new GameController();
+        gameController.startGame();
     }
 }
