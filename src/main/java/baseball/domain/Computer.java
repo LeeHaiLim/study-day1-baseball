0c2fbeb8b858caf7aaaad8bbffa2102430c395f7
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 12:50:20 +0900

    feat: Computer 객체 생성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> threeNumbers;
+
+    public Computer(List<Integer> threeNumbers) {
+        this.threeNumbers = threeNumbers;
+    }
+}
