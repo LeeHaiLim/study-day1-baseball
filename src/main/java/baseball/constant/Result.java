d295a8feb9db361d2c6909c13047b8bf2e8f3ae6
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 15:00:54 +0900

    feat: 비교 결과를 반환하는 Result enum 생성

---
+++ src/main/java/baseball/constant/Result.java
@@ -0,0 +1,17 @@
+package baseball.constant;
+
+import java.util.List;
+
+public enum Result {
+    STRIKE, BALL, NONE;
+
+    public static Result getResult(List<Integer> computerNumbers, int playerNumber, int index) {
+        if (computerNumbers.get(index)==playerNumber) {
+            return STRIKE;
+        }
+        if (computerNumbers.contains(playerNumber)) {
+            return BALL;
+        }
+        return NONE;
+    }
+}
