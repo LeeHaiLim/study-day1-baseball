e77773cf3d79139bf08f528dbf1ed0d74602d1cb
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:11:51 +0900

    feat(view): add method print result

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,26 @@
 package baseball.view;
 
+import static baseball.view.OutputMessage.GAME_START;
+import static baseball.view.OutputMessage.GAME_END;
+import static baseball.view.OutputMessage.BALL;
+import static baseball.view.OutputMessage.STRIKE;
+import static baseball.view.OutputMessage.NOTHING;
+
 public class OutputView {
-    public void printGameStart(){
-        System.out.println(OutputMessage.GAME_START.message());
+    public void printGameStart() {
+        System.out.println(GAME_START.message());
+    }
+
+    public void printResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.print(NOTHING.message());
+        }
+        if (ball > 0) {
+            System.out.printf(BALL.message(), ball);
+        }
+        if (strike > 0) {
+            System.out.printf(STRIKE.message(), strike);
+        }
+        System.out.println();
     }
 }
