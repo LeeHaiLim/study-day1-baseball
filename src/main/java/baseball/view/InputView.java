bb932ae853f26260dd853d1bd75ed62c19cba78a
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:28:41 +0900

    feat(view): add method read input game command

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,19 @@
 package baseball.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static baseball.view.InputMessage.NUMBER;
+import static baseball.view.InputMessage.GAME_COMMAND;
+
+import baseball.domain.GameCommand;
 
 public class InputView {
     public String readInputNumber() {
         System.out.print(InputMessage.NUMBER.message());
+        return readLine().trim();
+    }
+
+    public String readInputGameCommand() {
+        System.out.println(GAME_COMMAND.message());
         return readLine().trim();
     }
 }
