a27231e97464070dbe266a890971031179fbc1ba
EUNJEONGMUN <munej26@gmail.com>
2022-11-27 13:13:04 +0900

    feat: 사용자 입력 수가 1~9사이의 숫자가 아닐 경우 예외 발생 메서드 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,11 +1,27 @@
 package baseball.domain;
 
+import baseball.constant.ErrorMessage;
+
 import java.util.List;
+
+import static baseball.constant.Constant.*;
 
 public class Player {
     private final List<Integer> numbers;
 
     public Player(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    private void validNumberRange(String numbers) {
+        if (numbers.chars().noneMatch(num -> checkNumberRange((char) num))) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_RANGE.getMessage());
+        }
+    }
+    private boolean checkNumberRange(char number) {
+        if ((number-ZERO_CHAR)<NUMBER_RANGE_MIN || (number-ZERO_CHAR)>NUMBER_RANGE_MAX) {
+            return false;
+        }
+        return true;
     }
 }
