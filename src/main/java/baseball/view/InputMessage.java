1c0555e7e3981e0736fb06885d5ea9e6ba6c507e
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 13:17:29 +0900

    feat(view): implement enum class input message
    Added string constants:
    - number input message
    - quit or restart input message

---
+++ src/main/java/baseball/view/InputMessage.java
@@ -0,0 +1,21 @@
+package baseball.view;
+
+import static baseball.util.Constants.FLAG_GAME_RESTART;
+import static baseball.util.Constants.FLAG_GAME_QUIT;
+
+public enum InputMessage {
+
+    NUMBER("숫자를 입력해주세요 : "),
+    QUIT_OR_RESTART(String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.",
+            FLAG_GAME_RESTART, FLAG_GAME_QUIT));
+
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
