bb36246fc9f41f796091c57c9f5a26e49d6de7a9
Limjongho <whdgh9873@icloud.com>
2022-11-27 13:21:56 +0900

    test: 사용자의 숫자 반환 기능 관련 테스트 추가

--- src/test/java/baseball/domain/UserNumberTest.java
+++ src/test/java/baseball/domain/UserNumberTest.java
@@ -25,4 +25,23 @@ class UserNumberTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 숫자가 포함되어 있습니다.");
     }
+
+    @DisplayName("정상적인 숫자 리스트로 객체 생성 테스트")
+    @Test
+    void insertValidUserNumber() {
+        List<Integer> inputNumbers = List.of(1, 2, 3);
+        UserNumber userNumber = new UserNumber(inputNumbers);
+
+        Assertions.assertThat(userNumber.getUserNumbers().size()).isEqualTo(3);
+    }
+
+    @DisplayName("사용자의 숫자 객체에서 꺼낸 숫자 리스트를 수정하는 경우 예외가 발생한다.")
+    @Test
+    void changeUserNumber() {
+        List<Integer> inputNumbers = List.of(1, 2, 3);
+        UserNumber userNumber = new UserNumber(inputNumbers);
+
+        Assertions.assertThatThrownBy(() -> userNumber.getUserNumbers().add(1))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
 }
