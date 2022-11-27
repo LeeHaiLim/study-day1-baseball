ba408da982bf0bee06646fcacdf5dca63751252b
LeeHaiLim <hailim1997@gmail.com>
2022-11-27 15:45:04 +0900

    refactor(baseball): remove unused import

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import baseball.domain.GameCommand;
-
 public class Application {
     public static void main(String[] args) {
         BaseballController controller = new BaseballController();
